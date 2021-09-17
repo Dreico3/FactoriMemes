a7f34ebef565fe1f0392c0df55aca7df10499b57
Erick Cruz <fis9.81@gmail.com>
2021-09-17 17:03:58 -0400

    falta la captura y descarga de imagen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import './styles/letras.css'
 function App() {
+  const [inputs,setInputs]=React.useState(
+    {
+      linea1:'',
+      linea2:'',
+      image:'fire',
+    }
+  )
+  const Leer = (e) =>{
+    setInputs({...inputs,[e.target.name]:e.target.value})
+  }
+     
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <select onChange={Leer} name='image'>
+        <option value='fire'>fuego</option>
+        <option value='futurama'>Futurama</option>
+        <option value='history'>history</option>
+        <option value='matrix'>Matrix</option>
+        <option value='philosoraptor'>Dinosaurio</option>
+        <option value='smart'>negro pensando</option>
+      </select> 
+      <br/>
+      <input 
+        type='text' 
+        placeholder='linea 1'
+        onChange={Leer}
+        value={inputs.liena1}
+        name='linea1'
+      /><br/>
+      <input 
+        type='text' 
+        placeholder='linea 2'
+        onChange={Leer}
+        value={inputs.liena2}
+        name='linea2'
+      /><br/>
+      <button>exportar</button>
+      <div>
+        <span className='uno'>{inputs.linea1}</span><br/>
+        <span className='dos'>{inputs.linea2}</span><br/>
+        
+        <img src={`./images/${inputs.image}.jpg`} width='700' />
+      </div>
     </div>
   );
 }
